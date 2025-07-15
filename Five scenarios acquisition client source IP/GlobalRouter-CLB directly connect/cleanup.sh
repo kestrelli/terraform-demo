40bf3fac4dcf2a0a574241e55e8399dbbdba2a19
kestrelli <lhr1205146370@163.com>
2025-07-15 21:28:44 +0800

    GlobalRouter-CLB directly connect

---
+++ Five scenarios acquisition client source IP/GlobalRouter-CLB directly connect/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kubectl delete svc clb-direct-pod
+kubectl delete deploy real-ip-demo
+kubectl patch cm tke-service-controller-config -n kube-system --patch '{"data":{"GlobalRouteDirectAccess":"false"}}'
