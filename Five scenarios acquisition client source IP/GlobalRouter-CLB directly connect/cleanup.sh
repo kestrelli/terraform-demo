3942c9f2146c362c81a6f1a03a6949889cc5c637
kestrelli <lhr1205146370@163.com>
2025-07-15 21:27:37 +0800

    Delete Five scenarios acquisition client source IP/GlobalRouter-CLB directly connect directory

--- Five scenarios acquisition client source IP/GlobalRouter-CLB directly connect/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kubectl delete svc clb-direct-pod
-kubectl delete deploy real-ip-demo
-kubectl patch cm tke-service-controller-config -n kube-system --patch '{"data":{"GlobalRouteDirectAccess":"false"}}'
--- Five scenarios acquisition client source IP/GlobalRouter-CLB directly connect/verify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CLB_IP=$(kubectl get svc clb-direct-pod -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-curl -s http://$CLB_IP | grep remote_addr
